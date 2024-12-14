a42c354b110e90b112635fce0fc02353aff35215
Pramit Bhatia <pramitbhatia25@gmail.com>
2024-12-13 23:09:12 -0500

    Edit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), NodeGlobalsPolyfillPlugin({
+    process: true,
+  }),
+  NodeModulesPolyfillPlugin(),
+  ],
   resolve: {
     alias: {
       src: "/src",
